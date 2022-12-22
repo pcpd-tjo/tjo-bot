d823818651d068e00ad96570f787b6a3382d1715
UntoldGam <charliebird373@gmail.com>
2022-12-22 14:53:38 +0000

    another fix

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
   console.log(listener.address());
 });
+
+module.exports = app
--- setup.js
+++ setup.js
@@ -58,12 +58,9 @@ client.on('interactionCreate', async interaction => {
 });
 
 
-
 const listener = app.listen(process.env.PORT, () => {
 	console.log("Your app is listening on port " + listener.address().port);
 	console.log(listener.address());
 });
-
-
 
 module.exports = app
