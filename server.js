78455eb30b294201a5b57dec2c43c3d8a9da3bd7
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2023-09-10 22:27:00 +0100

    updates

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+export default async function () {    
+    app.get("/", (req, res) => {
+        if (res.statusCode == 200) {
+          res.json({ message: "TJO BOT ONLINE", status: res.statusCode});
+        } else {
+          res.sendStatus(200)
+        }
+        
+      });
+      
+      const listener = app.listen(3000, () => {
+        console.log("Your app is listening on port " + listener.address().port);
+        console.log(listener.address());
+      });
+}
