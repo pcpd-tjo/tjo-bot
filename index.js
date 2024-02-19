79a353ac00c6429e3939af130e698874d2b40917
UntoldGam <93488596+UntoldGam@users.noreply.github.com>
2024-02-19 22:39:33 +0000

    abstract command registration, fix prestart not running

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 /* eslint-disable no-undef */
 import { config } from 'dotenv';
-import { Client, Events, GatewayIntentBits, Collection, REST, Routes } from 'discord.js';
-import { readdirSync } from 'fs';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 import expressServer from "./server.js";
 
-import { dirname, join } from 'path';
-import { fileURLToPath } from 'url';
-
-import { clientId } from "./config.js";
 
 // allows process.env to be used during runtime
 config({ path: './.env' });
@@ -17,52 +12,8 @@ config({ path: './.env' });
 expressServer();
 
 const { TOKEN } = process.env;
-
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = new Collection();
-let commands = [];
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const commandsPath = join(__dirname, "commands");
-const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = join(commandsPath, file);
-	try {
-		const {default: command} = await import(`./commands/${file}`)
-		if (command.data) {
-			client.commands.set(command.data.name, command);
-			commands.push(command.data);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
-	} catch (error) {
-		console.error(`Error loading command at ${filePath}:`, error);
-	}
-}
-
-const rest = new REST({ version: '10' }).setToken(TOKEN);
-
-(async () => {
-	try {
-		console.log(`Started refreshing x application (/) commands.`);
-		commands = JSON.stringify(commands);
-		commands = JSON.parse(commands)
-
-		const data = await rest.put(
-			Routes.applicationCommands(clientId),
-			{ body: commands },
-		);
-
-		console.log(`Successfully reloaded ${data.length || "0"} application (/) commands.`);
-	} catch (error) {
-		console.error(error);
-	}
-})();
 
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
@@ -85,5 +36,4 @@ client.on(Events.InteractionCreate, async interaction => {
 	}
 });
 
-// Log in to Discord with your client's token
 client.login(TOKEN);
---
+++ registerCommands.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+import { REST, Routes } from 'discord.js';
+import { readdirSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+import { clientId } from "./config.js";
+const { TOKEN } = process.env;
+export const register = async () => {
+    let commands = [];
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const commandsPath = join(__dirname, "commands");
+    const commandFiles = readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+    for (const file of commandFiles) {
+        const filePath = join(commandsPath, file);
+        try {
+            const { default: command } = await import(`./commands/${file}`)
+            if (command.data) {
+                commands.push(command.data);
+            } else {
+                console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            }
+        } catch (error) {
+            console.error(`Error loading command at ${filePath}:`, error);
+        }
+    }
+
+    const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+    (async () => {
+        try {
+            console.log(`Started refreshing x application (/) commands.`);
+            commands = JSON.stringify(commands);
+            commands = JSON.parse(commands)
+
+            const data = await rest.put(
+                Routes.applicationCommands(clientId),
+                { body: commands },
+            );
+
+            console.log(`Successfully reloaded ${data.length || "0"} application (/) commands.`);
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+}
